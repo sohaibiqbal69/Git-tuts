03a4c755e914d30f8d7ff6e18bef304606648aa5
Sohaib Iqbal <sohaib.iqbal@somaiya.edu>
2023-07-02 23:45:24 +0530

    added Mutliply.java

---
+++ Multiply.java
@@ -0,0 +1,10 @@
+public class Multiply {
+    
+
+    public static void main(String args[]) {
+        int a = 12;
+        int b = 2;
+
+        System.out.println("The Product is" + (a*b));
+    }
+}
